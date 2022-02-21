a2482bc80863ed2c88e9400e2f3581792d5ee63b
Leandro-Barretoo <officialdevmi@gmail.com>
2022-02-21 09:50:30 +0000

    Add name field to sign up devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
 
-  # Defines the root path route ("/")
-  root "groups#index"
+  authenticated :user do
+    root to: 'groups#index', as: :authenticated_root
+  end
+
+  resources :groups
+  resources :buys
 end
