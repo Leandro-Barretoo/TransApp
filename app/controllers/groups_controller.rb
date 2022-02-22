1b5652e1732c2528c0f94cce90fb0e0bbded6028
Leandro-Barretoo <officialdevmi@gmail.com>
2022-02-22 17:52:17 +0000

    Add categories page and form

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,4 +4,26 @@ class GroupsController < ApplicationController
     def index
         @groups = current_user.groups
     end
+
+    def new
+        @group = Group.new
+    end
+
+    def create
+        @group = current_user.groups.new(group_params)
+    
+        respond_to do |format|
+          if @group.save
+            format.html { redirect_to root_path, notice: 'Category was successfully created.' }
+          else
+            format.html { render :new, status: :unprocessable_entity }
+          end
+        end
+    end
+
+    private
+
+    def group_params
+        params.require(:group).permit(:name, :icon)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
 
-  authenticated :user do
-    root to: 'groups#index', as: :authenticated_root
-  end
-
   resources :groups
   resources :buys
 end
